079da29e0593b22b8fd981c1e8ecba6425ebea66
Adalat222 <adalatkumar2580@gmail.com>
2022-12-31 20:17:25 +0530

    Testing

--- clone.sh
+++ clone.sh
@@ -1,4 +1,5 @@
-echo y | repo init --depth=1 -u https://github.com/Evolution-X/manifest -b tiramisu
+echo y | repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-20.0
 repo sync -c -j5 --force-sync --no-clone-bundle --no-tags --fail-fast
-git clone --depth=1 https://github.com/DevAdalat/android_device_xiaomi_gauguin device/xiaomi/gauguin
+git clone --depth=1 https://github.com/Molyuu/device_xiaomi_gauguin -b lineage-20.0 device/xiaomi/gauguin
+git clone --depth=1 https://github.com/Molyuu/vendor_xiaomi_gauguin vendor/xiaomi/gauguin
 . build/envsetup.sh
