1936d154b0f3e03d5ddceea6a10fa4813250c105
DevAdalat <akbusiness022@gmail.com>
2023-01-03 06:24:50 +0530

    Build

--- clone.sh
+++ clone.sh
@@ -1,8 +1,6 @@
 echo y | repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-20.0
-repo sync -c -j5 --force-sync --no-clone-bundle --no-tags --fail-fast
+repo sync -c -j1 --force-sync --no-clone-bundle --no-tags --fail-fast
 git clone --depth=1 https://github.com/LineageOS/android_device_xiaomi_gauguin device/xiaomi/gauguin
-git clone --depth=1 https://github.com/LineageOS/android_kernel_xiaomi_gauguin kernel/xiaomi/gauguin
-git clone --depth=1 https://github.com/Molyuu/vendor_xiaomi_gauguin vendor/xiaomi/gauguin
 export USE_CCACHE=1
 ccache -M 20G
 ccache -o compression=true
