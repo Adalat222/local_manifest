d076e923b32230b4203dccc06a7b9a75d0bfb234
DevAdalat <akbusiness022@gmail.com>
2023-01-04 12:10:43 +0530

    Testing

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault 
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync -c -j8 --force-sync --no-clone-bundle --no-tags
 git clone --depth=1 https://github.com/iitzrohan/device_xiaomi_gauguin device/xiaomi/gauguin
 
 . build/envsetup.sh
