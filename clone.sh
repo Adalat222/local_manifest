09cb7d92dc20f0d90c3e1107c6af27379a2804a3
DevAdalat <akbusiness022@gmail.com>
2023-01-06 13:02:44 +0530

    Testing

--- clone.sh
+++ clone.sh
@@ -1,8 +1,3 @@
 echo y | repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
 repo sync -c -j1 --force-sync --no-clone-bundle --no-tags --fail-fast
-git clone --depth=1 https://github.com/LineageOS/android_device_xiaomi_gauguin device/xiaomi/gauguin
-git clone --depth=1 https://github.com/Molyuu/vendor_xiaomi_gauguin vendor/xiaomi/gauguin
-export USE_CCACHE=1
-ccache -M 20G
-ccache -o compression=true
-. build/envsetup.sh && breakfast gauguin && croot && brunch gauguin
+git clone --depth=1 https://github.com/Adalat222/android_device_xiaomi_gauguin device/xiaomi/gauguin
