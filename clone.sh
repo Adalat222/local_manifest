f862b71579f821fd4faa7edd32efc72eb8f13d78
DevAdalat <akbusiness022@gmail.com>
2023-01-06 13:24:01 +0530

    Testing

--- clone.sh
+++ clone.sh
@@ -1,3 +1,3 @@
 echo y | repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
-repo sync -c -j1 --force-sync --no-clone-bundle --no-tags --fail-fast
+repo sync -c -j8 --force-sync --no-clone-bundle --no-tags --fail-fast
 git clone --depth=1 https://github.com/Adalat222/android_device_xiaomi_gauguin device/xiaomi/gauguin
